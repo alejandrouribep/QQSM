5128a6c2b4ba1b3b7225a5952f6005cb3e010702
Alejandro Uribe <alejandrouribep@gmail.com>
2013-10-31 13:38:29 -0500

    CLASE JUEGO Y PREGUNTAS

---
+++ pregunta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQSM
+{
+    class pregunta
+    {
+        private string enunciado;
+        private string [] opciones;
+        private string respuesta;
+        private int puntaje;
+
+        public pregunta(string enunciado, string respuesta,string []opciones,int puntaje)
+        {
+            this.enunciado = enunciado;
+            this.respuesta = respuesta;
+            this.opciones = opciones;
+            this.puntaje = puntaje;
+ 
+        }
+        
+
+
+
+    }
+}
